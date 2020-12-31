6b060341d1969d078bc2c1641e453735f86db1ae
Saryn Mooney <smooney15@my.whitworth.edu>
2020-12-30 19:41:08 -0700

    return missing element

--- code_wars/practice.rb
+++ code_wars/practice.rb
@@ -1,3 +1,11 @@
+# Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+#
+# Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+def get_missing_element(seq)
+  numbers = (0..9).to_a
+  result = (numbers - seq).join.to_i
+end
+
 def to_time(seconds)
   hours = seconds / 3600
   minutes = (seconds / 60) % 60
