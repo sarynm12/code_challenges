868d31afa3ee5136bc0c213b596f2908ee61707c
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-15 21:16:07 -0700

    complete recursive method

--- code_wars/practice.rb
+++ code_wars/practice.rb
@@ -1,3 +1,20 @@
+# Given a deeply nested array, yyour goal is to recreate this functionality by writing a recursive function to accomplish this same thing. For example:
+#
+#  array_of_ints = [1, 2, 3, [[4], 5], [[[6]]]]
+# => [1, 2, 3, [[4], 5], [[[6]]]]
+# ruby_flattener(array_of_ints)
+# => [1, 2, 3, 4, 5, 6]
+def flatten(ints, arr=[])
+  ints.each do |int|
+    if int.class != Array
+      arr << int
+    else
+      flatten(int, arr)
+    end
+  end
+  arr
+end 
+
 # Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
 #
 # Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
@@ -5,13 +22,6 @@ def get_missing_element(seq)
   numbers = (0..9).to_a
   result = (numbers - seq).join.to_i
 end
-
-def to_time(seconds)
-  hours = seconds / 3600
-  minutes = (seconds / 60) % 60
-  "#{hours} hour(s) and #{minutes} minute(s)"
-end
-
 # Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
 #
 # Only numbers without decimals like 4 or 4.0 can be even.
@@ -37,4 +47,10 @@ def solve(arr)
   end
 
   result.compact
+end
+
+def to_time(seconds)
+  hours = seconds / 3600
+  minutes = (seconds / 60) % 60
+  "#{hours} hour(s) and #{minutes} minute(s)"
 end
