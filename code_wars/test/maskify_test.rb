fa8821d13a7316941bb3222efbdf01da92906090
Saryn Mooney <smooney15@my.whitworth.edu>
2021-04-06 09:46:50 -0600

    add to spec

--- code_wars/test/maskify_test.rb
+++ code_wars/test/maskify_test.rb
@@ -3,6 +3,16 @@ require './lib/maskify'
 
 class MaskifyTest < Minitest::Test 
 
+  def test_it_exists 
+    maskify = Maskify.new('abcd')
+    assert_instance_of Maskify, maskify 
+  end 
+
+  def test_it_can_maskify
+    maskify = Maskify.new('4556364607935616')
+    assert_equal '############5616', maskify.encrypt
+  end 
+
 end 
 
 # maskify('4556364607935616') # should return '############5616'
